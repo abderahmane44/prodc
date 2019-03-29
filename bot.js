a984928f43d7f1ac02516f707fdab38dd76bb1bf
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-03-29 01:42:55 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,29 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
+const invites = {};
+const wait = require('util').promisify(setTimeout);
+client.on('ready', () => {
+  wait(1000);
+  client.guilds.forEach(king => {
+    king.fetchInvites().then(guildInvites => {
+      invites[king.id] = guildInvites;
+    });
+  });
+});
 
-client.on("ready", () => {
-let channel =     client.channels.get("")
-setInterval(function() {
-channel.send(``);
-}, 30)
-})
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const gamer = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => gamer.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const welcome = member.guild.channels.find(channel => channel.name === "chat");
+    welcome.send(` ||${member.user.tag}|| invited by ||${inviter.tag}|| invites =  ||${invite.uses}|| `)
+  });
+});
+
+
+
 
 client.login(process.env.BOT_TOKEN);
